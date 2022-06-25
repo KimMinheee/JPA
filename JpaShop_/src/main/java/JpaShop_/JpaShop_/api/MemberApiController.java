baf30fc93f361a8ad4a5fd73968d68064a412891
KimMinheee <99alsgmlalsl@naver.com>
2022-06-25 18:59:13 +0900

    [ADD] 회원 정보 수정 api

--- JpaShop_/src/main/java/JpaShop_/JpaShop_/api/MemberApiController.java
+++ JpaShop_/src/main/java/JpaShop_/JpaShop_/api/MemberApiController.java
@@ -3,11 +3,10 @@ package JpaShop_.JpaShop_.api;
 
 import JpaShop_.JpaShop_.domain.Member;
 import JpaShop_.JpaShop_.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -17,8 +16,7 @@ public class MemberApiController {
     private final MemberService memberService;
 
     /*
-    * 회원 등록 v1 : 요청값으로 Member 엔티티를 직접 받는다.
-    *
+    * [POST] : 회원 등록 v1 : 요청값으로 Member 엔티티를 직접 받는다.
      */
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
@@ -26,6 +24,10 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+
+    /*
+     * [POST] :  회원 등록 v2 : 요청값으로 CreateMemberRequest DTO 생성
+     */
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
         Member member = new Member();
@@ -34,19 +36,41 @@ public class MemberApiController {
         return new CreateMemberResponse(id);
     }
 
+    /*
+     * [PUT] : 회원 수정 v2
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,  @RequestBody @Valid UpdateMemberRequest request){
+        memberService.update(id, request.getName()); //수정
+        Member findMember = memberService.findOne(id); //조회
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
 
 
     @Data
     static class CreateMemberRequest{
         private String name;
     }
+
     @Data
     static class CreateMemberResponse{
         private Long id;
         public CreateMemberResponse(Long id){
             this.id=id;
         }
+    }
 
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
     }
 
 
--- JpaShop_/src/main/java/JpaShop_/JpaShop_/service/MemberService.java
+++ JpaShop_/src/main/java/JpaShop_/JpaShop_/service/MemberService.java
@@ -49,5 +49,13 @@ public class MemberService {
         return memberRepository.findOne(memberId);
     }
 
+    /*
+    * 회원 수정(name)
+     */
+    @Transactional
+    public void update(Long id, String name){
+        Member member = memberRepository.findOne(id);
+        member.setName(name);
+    }
 
 }
